70480424f3aa95548e4b87f407d5e468de7fc9d7
Lucas Rey <lrey4@bloomberg.net>
2022-10-17 09:12:30 +0100

    basic add an mult kinda working

--- public/adhoc.hpp
+++ public/adhoc.hpp
@@ -1,4 +1,114 @@
 #ifndef ADHOC_HPP
 #define ADHOC_HPP
 
+namespace adhoc {
+
+template <class Input> class mul_scalar;
+
+template <class Input> class add_scalar {
+    double m_scalar{0.};
+    Input &m_active;
+
+  public:
+    add_scalar(double scalar, Input &active);
+    auto operator+(double rhs) const -> add_scalar<const add_scalar<Input>>;
+    auto operator*(double rhs) const -> mul_scalar<const add_scalar<Input>>;
+    operator double() const noexcept;
+
+    template <class Denom> auto d(Denom &in) const noexcept -> double;
+};
+
+template <class Input>
+add_scalar<Input>::add_scalar(double scalar, Input &active)
+    : m_scalar(scalar), m_active(active) {}
+template <class Input>
+auto add_scalar<Input>::operator+(double rhs) const
+    -> add_scalar<const add_scalar<Input>> {
+    return {rhs, *this};
+}
+template <class Input>
+auto add_scalar<Input>::operator*(double rhs) const
+    -> mul_scalar<const add_scalar<Input>> {
+    return {rhs, *this};
+}
+template <class Input> add_scalar<Input>::operator double() const noexcept {
+    return this->m_scalar + this->m_active.operator double();
+}
+
+template <class Input>
+template <class Denom>
+inline auto add_scalar<Input>::d(Denom &in) const noexcept -> double {
+    return this->m_active.d(in);
+}
+
+template <class Input> class mul_scalar {
+    double m_scalar{0.};
+    Input &m_active;
+
+  public:
+    mul_scalar(double scalar, Input &active);
+
+    auto operator+(double rhs) const -> add_scalar<const mul_scalar<Input>>;
+    auto operator*(double rhs) const -> mul_scalar<const mul_scalar<Input>>;
+    operator double() const noexcept;
+
+    template <class Denom> auto d(Denom &in) const noexcept -> double;
+};
+
+template <class Input>
+mul_scalar<Input>::mul_scalar(double scalar, Input &active)
+    : m_scalar(scalar), m_active(active) {}
+template <class Input>
+auto mul_scalar<Input>::operator+(double rhs) const
+    -> add_scalar<const mul_scalar<Input>> {
+    return {rhs, *this};
+}
+template <class Input>
+auto mul_scalar<Input>::operator*(double rhs) const
+    -> mul_scalar<const mul_scalar<Input>> {
+    return {rhs, *this};
+}
+template <class Input> mul_scalar<Input>::operator double() const noexcept {
+    return this->m_scalar * this->m_active.operator double();
+}
+
+template <class Input>
+template <class Denom>
+inline auto mul_scalar<Input>::d(Denom &in) const noexcept -> double {
+    return this->m_scalar * this->m_active.d(in);
+}
+
+class adouble {
+    double m_value{0};
+
+  public:
+    adouble(double value);
+    auto operator+(double rhs) const -> add_scalar<const adouble>;
+    auto operator*(double rhs) const -> mul_scalar<const adouble>;
+
+    operator double() const noexcept;
+
+    template <class Denom> auto d(Denom &in) const noexcept -> double;
+};
+
+inline adouble::adouble(double value) : m_value(value) {}
+inline auto adouble::operator+(double rhs) const -> add_scalar<const adouble> {
+    return {rhs, *this};
+}
+inline auto adouble::operator*(double rhs) const -> mul_scalar<const adouble> {
+    return {rhs, *this};
+}
+inline adouble::operator double() const noexcept { return this->m_value; }
+
+template <class Denom>
+inline auto adouble::d(Denom & /* in */) const noexcept -> double {
+    return 0.;
+}
+
+template <> inline auto adouble::d(adouble &in) const noexcept -> double {
+    return static_cast<double>(&in == this);
+}
+
+} // namespace adhoc
+
 #endif // ADHOC_HPP
--- tests/test.cpp
+++ tests/test.cpp
@@ -2,7 +2,50 @@
 
 #include <gtest/gtest.h>
 
+namespace adhoc {
+
 TEST(Test, Initial) {
-    double val = 0.;
-    EXPECT_EQ(val, 0.);
+    adouble val(1.);
+    auto valplus = val + 1.0;
+    auto valmult = valplus * 2.0;
+    auto valmult2 = (valmult + 2.0) * 3.0;
+    double result = valmult2;
+    // double deriv = d(result) / d(val);
+    // double deriv = d(result, val);
+    EXPECT_EQ(result, 18.);
+    // EXPECT_EQ(deriv, 0.);
 }
+
+TEST(Test, doperator) {
+    adouble val1(1.);
+    adouble val2(2.);
+    auto valplus = val1 + 1.0;
+    double res1 = val1.d(val1);
+    double res2 = val1.d(val2);
+    double res3 = val1.d(valplus);
+    EXPECT_EQ(res1, 1.);
+    EXPECT_EQ(res2, 0.);
+    EXPECT_EQ(res3, 0.);
+}
+
+TEST(Test, doperatorplus) {
+    adouble val1(1.);
+    adouble val2(2.);
+    auto valplus = val1 + 1.0;
+    double res1 = valplus.d(val1);
+    double res2 = valplus.d(val2);
+    EXPECT_EQ(res1, 1.);
+    EXPECT_EQ(res2, 0.);
+}
+
+TEST(Test, doperatormult) {
+    adouble val1(1.);
+    adouble val2(2.);
+    auto valmul = val1 * 2.0;
+    double res1 = valmul.d(val1);
+    double res2 = valmul.d(val2);
+    EXPECT_EQ(res1, 2.);
+    EXPECT_EQ(res2, 0.);
+}
+
+} // namespace adhoc
