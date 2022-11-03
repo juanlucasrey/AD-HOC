3348d6f324a3946c95a1051c3aa7e8d3b8f03a98
Lucas Rey <lrey4@bloomberg.net>
2022-11-03 06:28:18 +0000

    nttp test

--- tests/nttp_final.cpp
+++ tests/nttp_final.cpp
@@ -26,31 +26,59 @@ template <class T> constexpr std::string_view type_name() {
 #endif
 }
 
+namespace detail {
 template <typename T> struct AsTemplateArg {
     T buffer{};
     constexpr AsTemplateArg(T t) : buffer(t) {}
     constexpr operator T() const { return this->buffer; }
 };
+} // namespace detail
 
-template <typename T, AsTemplateArg<T> F> struct C {
+template <typename T, detail::AsTemplateArg<T> F> struct C {
     constexpr operator T() const { return F; }
 };
 
+template <detail::AsTemplateArg<float> F> using CF = C<float, F>;
+template <detail::AsTemplateArg<double> F> using CD = C<double, F>;
+template <detail::AsTemplateArg<long double> F> using CLD = C<long double, F>;
+
 TEST(adhoc2, nttp_final) {
-    C<double, 0.4> tests7;
-    C<float, 0.6f> tests8;
-    C<long double, 0.4l> tests9;
+    float constexpr onef = 1.0f;
+    C<float, 1.0f> onef_type;
+    // different sizes, type is empty
+    static_assert(sizeof(onef) == 4);
+    static_assert(sizeof(onef_type) == 1);
+    // same values
+    static_assert(onef == onef_type);
+
+    double constexpr oned = 1.0;
+    C<double, 1.0> oned_type;
+    // different sizes, type is empty
+    static_assert(sizeof(oned) == 8);
+    static_assert(sizeof(oned_type) == 1);
+    // same values
+    static_assert(oned == oned_type);
+
+    long double constexpr oneld = 1.0l;
+    C<long double, 1.0l> oneld_type;
+    // different sizes, type is empty
+    static_assert(sizeof(oneld) == 16);
+    static_assert(sizeof(oneld_type) == 1);
+    // same values
+    static_assert(oneld == oneld_type);
 
     double constexpr one = 1.0;
-    C<double, one> one_type;
-    C<double, one> one_type2;
+    CD<one> one_type;
+    CD<one> one_type2;
+    // same type for same values
     static_assert(std::is_same_v<decltype(one_type), decltype(one_type)>);
     static_assert(one == one_type);
+    static_assert(one == one_type2);
+
+    // 1.000000000000000222 = std::nextafter(1)
     double constexpr oneeps = 1.000000000000000222;
-    C<double, oneeps> oneeps_type;
+    CD<oneeps> oneeps_type;
+    // different types for epsilon
     static_assert(!std::is_same_v<decltype(one_type), decltype(oneeps_type)>);
     static_assert(oneeps == oneeps_type);
-
-    std::cout << sizeof(one) << std::endl;
-    std::cout << sizeof(one_type) << std::endl;
 }
