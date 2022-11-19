598ac3e7935a7e81e50ae0f9c4ae53dcb476e429
Lucas Rey <lrey4@bloomberg.net>
2022-11-19 22:43:05 +0000

    tuple utils

---
+++ public/new/tuple_utils.hpp
@@ -0,0 +1,47 @@
+#ifndef ADHOC_TUPLE_UTILS_HPP
+#define ADHOC_TUPLE_UTILS_HPP
+
+#include "utils.hpp"
+
+#include <iostream>
+#include <tuple>
+
+namespace adhoc2 {
+
+namespace detail {
+
+template <std::size_t IdxToReplace, class Replacement, class... TupleValues,
+          std::size_t... IndicesStart, std::size_t... IndicesEnd>
+auto constexpr replace_first_aux2(std::tuple<TupleValues...> /* in */,
+                                  std::index_sequence<IndicesStart...>,
+                                  std::index_sequence<IndicesEnd...>) {
+    return std::make_tuple(
+        std::tuple_element_t<IndicesStart, std::tuple<TupleValues...>>{}...,
+        Replacement{},
+        std::tuple_element_t<IndicesEnd + IdxToReplace + 1,
+                             std::tuple<TupleValues...>>{}...);
+}
+
+template <std::size_t IdxToReplace, class Replacement, class... TupleValues,
+          class IndicesStart = std::make_index_sequence<IdxToReplace>,
+          class IndicesEnd = std::make_index_sequence<sizeof...(TupleValues) -
+                                                      IdxToReplace - 1>>
+auto constexpr replace_first_aux(std::tuple<TupleValues...> /* in */) {
+    return replace_first_aux2<IdxToReplace, Replacement>(
+        std::tuple<TupleValues...>{}, IndicesStart{}, IndicesEnd{});
+}
+
+} // namespace detail
+
+template <class TypeToReplace, class Replacement, class... TupleValues>
+auto constexpr replace_first(std::tuple<TupleValues...> /* in */) {
+    static_assert(has_type2<TypeToReplace, TupleValues...>(),
+                  "type to replace not on this tuple");
+    constexpr auto idx =
+        detail::get_index<TypeToReplace, TupleValues...>::value;
+    return detail::replace_first_aux<idx, Replacement>(
+        std::tuple<TupleValues...>{});
+}
+
+} // namespace adhoc2
+#endif // ADHOC_TUPLE_UTILS_HPP
---
+++ tests/new/tuple_utils.t.cpp
@@ -0,0 +1,58 @@
+#include <new/tuple_utils.hpp>
+
+#include "../type_name.hpp"
+
+#include <gtest/gtest.h>
+
+namespace adhoc2 {
+
+class available_t {};
+class replacement_t {};
+class node1 {};
+class node2 {};
+class node3 {};
+class node4 {};
+class node5 {};
+class node6 {};
+
+TEST(TupleUitls, First) {
+    auto constexpr res3 = replace_first<available_t, replacement_t>(
+        std::tuple<available_t, node1, node2, node3, node4, node5, node6>{});
+
+    static_assert(std::is_same_v<decltype(res3),
+                                 const std::tuple<replacement_t, node1, node2,
+                                                  node3, node4, node5, node6>>);
+}
+
+TEST(TupleUitls, Middle) {
+    auto constexpr res3 = replace_first<available_t, replacement_t>(
+        std::tuple<node1, node2, node3, available_t, node4, node5, node6>{});
+
+    static_assert(
+        std::is_same_v<decltype(res3),
+                       const std::tuple<node1, node2, node3, replacement_t,
+                                        node4, node5, node6>>);
+}
+
+TEST(TupleUitls, End) {
+    auto constexpr res3 = replace_first<available_t, replacement_t>(
+        std::tuple<node1, node2, node3, node4, node5, node6, available_t>{});
+
+    static_assert(
+        std::is_same_v<decltype(res3),
+                       const std::tuple<node1, node2, node3, node4, node5,
+                                        node6, replacement_t>>);
+}
+
+TEST(TupleUitls, Repeat) {
+    auto constexpr res3 = replace_first<available_t, replacement_t>(
+        std::tuple<available_t, available_t, node1, node2, node3, node4, node5,
+                   node6>{});
+
+    static_assert(
+        std::is_same_v<decltype(res3),
+                       const std::tuple<replacement_t, available_t, node1,
+                                        node2, node3, node4, node5, node6>>);
+}
+
+} // namespace adhoc2
