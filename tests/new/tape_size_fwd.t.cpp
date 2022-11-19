564469042886192530aa8f166bc118b0ba97c33b
Lucas Rey <lrey4@bloomberg.net>
2022-11-19 11:39:36 +0000

    evaluate_fwd

--- tests/new/tape_size_fwd.t.cpp
+++ tests/new/tape_size_fwd.t.cpp
@@ -1,5 +1,5 @@
 #include <new/calc_order.hpp>
-#include <new/evaluate.hpp>
+#include <new/evaluate_fwd.hpp>
 #include <new/init.hpp>
 #include <new/tape_nodes.hpp>
 #include <new/tape_size_bwd.hpp>
@@ -98,6 +98,7 @@ auto call_price(const I1 &S, const I2 &K, const I3 &v, const I4 &T) {
     auto d1 = log(S / K) / totalvol + totalvol * CD<encode(0.5)>();
     auto d2 = d1 + totalvol;
     return S * cdf_n(d1) - K * cdf_n(d2);
+    // return ((S * K) * v) * T;
 }
 
 TEST(TapeSizeFwd, BSSinglePrice) {
@@ -126,15 +127,53 @@ TEST(TapeSizeFwd, BSSinglePrice) {
 
     auto tape_d = TapeDerivatives(S, K, v, T, result_adhoc);
 
+    // auto result_adhoc2 = call_price(S, K, v, T);
+    auto result_adhoc2 = cdf_n(S);
     auto temp = tape_size_bwd<decltype(S), decltype(K), decltype(v),
-                              decltype(T), decltype(result_adhoc)>();
+                              decltype(T), decltype(result_adhoc2)>();
 
-    std::cout << type_name<decltype(temp)>() << std::endl;
+    std::cout << temp << std::endl;
+    // std::cout << type_name<decltype(temp)>() << std::endl;
     // auto int_tape = evaluate_fwd_return_vals(tape);
     // auto totalvol = v * sqrt(T);
     // double totalvol_val = int_tape.get(totalvol);
     // auto totalvol_val2 = 0.15 * std::sqrt(0.5);
     // EXPECT_EQ(totalvol_val, totalvol_val2);
+}
+
+template <class D> inline auto cdf_n2(D const &x) {
+    using constants::CD;
+    using constants::encode;
+    using std::erfc;
+    constexpr double minus_one_over_root_two =
+        -1.0 / constexpression::sqrt(2.0);
+    return CD<encode(0.5)>() *
+           erfc(x * CD<encode(minus_one_over_root_two)>()); // 2
+}
+
+template <class I1, class I2, class I3, class I4>
+auto call_price2(const I1 &S, const I2 &K, const I3 &v, const I4 &T) {
+    using constants::CD;
+    using constants::encode;
+    using std::log;
+    using std::sqrt;
+    auto totalvol = v * sqrt(T);
+    auto d1 = log(S / K) / totalvol + totalvol * CD<encode(0.5)>();
+    auto d2 = d1 + totalvol;
+    return S * cdf_n(d1) - K * cdf_n(d2);
+    // return v * T + (v * T - v * T);
+}
+
+TEST(TapeSizeFwd, BSSinglePrice2) {
+    auto [S, K, v, T] = Init<4>();
+    auto result_adhoc2 = call_price2(S, K, v, T);
+
+    auto nodes_bwd = calc_order_t<false>(result_adhoc2);
+
+    auto temp = tape_size_bwd<decltype(S), decltype(K), decltype(v),
+                              decltype(T), decltype(result_adhoc2)>();
+
+    std::cout << temp << std::endl;
 }
 
 TEST(TapeSizeBwd, CutLeafs) {
@@ -144,9 +183,9 @@ TEST(TapeSizeBwd, CutLeafs) {
                               decltype(v3), decltype(result)>();
     auto temp2 = tape_size_bwd<decltype(v0), decltype(v1), decltype(result)>();
 
-    std::cout << type_name<decltype(temp)>() << std::endl;
+    std::cout << temp << std::endl;
 
-    std::cout << type_name<decltype(temp2)>() << std::endl;
+    std::cout << temp2 << std::endl;
 }
 
 } // namespace adhoc2
