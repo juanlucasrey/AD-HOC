fb9474ca0aed4e747d09ad19f33144ffae29fd2f
Lucas Rey <lrey4@bloomberg.net>
2022-12-01 11:32:13 +0000

    bs cpp

--- tests/new/BS.t.cpp
+++ tests/new/BS.t.cpp
@@ -1,38 +1,10 @@
-#include <adhoc.hpp>
-#include <constants_constexpr.hpp>
-#include <constants_type.hpp>
-#include <erf_inv.hpp>
-#include <evaluate_bwd.hpp>
-#include <evaluate_fwd.hpp>
-#include <tape_static.hpp>
+#include <new/erf_inv.hpp>
 
-#include "type_name.hpp"
+#include "call_price.hpp"
 
 #include <gtest/gtest.h>
 
-namespace adhoc {
-
-template <class D> inline auto cdf_n(D const &x) {
-    using constants::CD;
-    using constants::encode;
-    using std::erfc;
-    constexpr double minus_one_over_root_two =
-        -1.0 / constexpression::sqrt(2.0);
-    return CD<encode(0.5)>() * erfc(x * CD<encode(minus_one_over_root_two)>());
-    // return CD<0.5>() * erfc(x * CD<minus_one_over_root_two>());
-}
-
-template <class I1, class I2, class I3, class I4>
-auto call_price(const I1 &S, const I2 &K, const I3 &v, const I4 &T) {
-    using constants::CD;
-    using constants::encode;
-    using std::log;
-    using std::sqrt;
-    auto totalvol = v * sqrt(T);
-    auto d1 = log(S / K) / totalvol + totalvol * CD<encode(0.5)>();
-    auto d2 = d1 + totalvol;
-    return S * cdf_n(d1) - K * cdf_n(d2);
-}
+namespace adhoc2 {
 
 TEST(BlackScholes, erfcinverse) {
     double x = 0.5;
@@ -106,33 +78,4 @@ TEST(BlackScholes, Inverse) {
     EXPECT_NEAR(d2, d2_inv, 1e-12);
 }
 
-TEST(BlackScholes, SinglePrice) {
-    double result = 0.;
-
-    {
-        double S = 100.0;
-        double K = 102.0;
-        double v = 0.15;
-        double T = 0.5;
-        result = call_price(S, K, v, T);
-    }
-
-    auto tape = CreateTapeInitial<4>();
-    auto [S, K, v, T] = tape.getalias();
-    tape.set(S, 100.0);
-    tape.set(K, 102.0);
-    tape.set(v, 0.15);
-    tape.set(T, 0.5);
-    auto result_adhoc = call_price(S, K, v, T);
-
-    auto intermediate_tape = CreateTapeIntermediate(result_adhoc);
-    evaluate_fwd(tape, intermediate_tape);
-
-    double result2 = intermediate_tape.get(result_adhoc);
-    EXPECT_EQ(result2, result);
-
-    auto derivatives =
-        evaluate_bwd(tape, intermediate_tape, result_adhoc, 1.0, S, K, v, T);
-}
-
-} // namespace adhoc
+} // namespace adhoc2
