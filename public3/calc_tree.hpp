18394c535355d96c6ebb16bfb113948374ac64f9
Lucas Rey <lrey4@bloomberg.net>
2024-02-03 11:42:50 +0000

    simplification

--- public3/calc_tree.hpp
+++ public3/calc_tree.hpp
@@ -17,23 +17,6 @@ template <class... Roots> class CalcTree {
 
     std::array<double, std::tuple_size<ValuesTuple>{}> m_values{};
 
-  public:
-    explicit CalcTree() = default;
-    explicit CalcTree(Roots... /* out */) {}
-    template <class Derived> auto inline val(Base<Derived> var) const -> double;
-    template <class Input> auto inline set(Input var) -> double &;
-
-  private:
-    template <constants::ArgType D>
-    inline auto val_aux(constants::CD<D> /* node */) -> double {
-        return constants::CD<D>::v();
-    }
-
-    template <class Node> inline auto val_aux(Node /* node */) -> double {
-        constexpr auto idx = get_idx_first2<Node>(ValuesTuple{});
-        return this->m_values[idx];
-    }
-
     inline void evaluate_fwd_aux(std::tuple<> /* nodes */) {}
 
     template <template <class> class Univariate, class Node,
@@ -43,7 +26,7 @@ template <class... Roots> class CalcTree {
     */) {
         using CurrentNode = Univariate<Node>;
         constexpr auto idx = get_idx_first2<CurrentNode>(ValuesTuple{});
-        this->m_values[idx] = CurrentNode::v(this->val_aux(Node{}));
+        this->m_values[idx] = CurrentNode::v(this->val(Node{}));
         this->evaluate_fwd_aux(std::tuple<NodesToCalc...>{});
     }
 
@@ -54,33 +37,37 @@ template <class... Roots> class CalcTree {
         using CurrentNode = Bivariate<Node1, Node2>;
         constexpr auto idx = get_idx_first2<CurrentNode>(ValuesTuple{});
         this->m_values[idx] =
-            CurrentNode::v(this->val_aux(Node1{}), this->val_aux(Node2{}));
+            CurrentNode::v(this->val(Node1{}), this->val(Node2{}));
         this->evaluate_fwd_aux(std::tuple<NodesToCalc...>{});
     }
 
   public:
+    explicit CalcTree() = default;
+    explicit CalcTree(Roots... /* out */) {}
+
+    template <constants::ArgType D>
+    auto inline val(constants::CD<D> /* in */) const -> double {
+        return constants::CD<D>::v();
+    }
+
+    template <class Input> auto inline val(Input /* in */) const -> double {
+        constexpr auto idx = get_idx_first2<Input>(ValuesTuple{});
+        return this->m_values[idx];
+    }
+
+    template <class Input> auto inline set(Input /* in */) -> double & {
+        static_assert(has_type_tuple<Input, leafs>::value,
+                      "only leafs are allowed to be set");
+        constexpr auto idx = get_idx_first2<Input>(ValuesTuple{});
+        return this->m_values[idx];
+    }
+
     inline auto evaluate() {
         auto constexpr calcs =
             detail::calc_order_aux_t<false, true, Roots...>::template call();
         this->evaluate_fwd_aux(calcs);
     }
 };
-
-template <class... Roots>
-template <class Derived>
-auto CalcTree<Roots...>::val(Base<Derived> /* in */) const -> double {
-    constexpr auto idx = get_idx_first2<Derived>(ValuesTuple{});
-    return this->m_values[idx];
-}
-
-template <class... Roots>
-template <class Input>
-auto CalcTree<Roots...>::set(Input /* in */) -> double & {
-    static_assert(has_type_tuple<Input, leafs>::value,
-                  "only leafs are allowed to be set");
-    constexpr auto idx = get_idx_first2<Input>(ValuesTuple{});
-    return this->m_values[idx];
-}
 
 } // namespace adhoc3
 
