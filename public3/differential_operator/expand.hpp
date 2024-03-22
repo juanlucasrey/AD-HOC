823423f3bee893c9da929d1a76c8da995b9dca4b
lrey4 <lrey4@bloomberg.net>
2024-03-22 16:06:59 +0000

    multiply ordered rework

--- public3/differential_operator/expand.hpp
+++ public3/differential_operator/expand.hpp
@@ -5,6 +5,7 @@
 #include "../constants_type.hpp"
 #include "../dependency.hpp"
 #include "differential_operator.hpp"
+#include "integer_partition_univariate_function.hpp"
 #include "is_ordered.hpp"
 #include "merge_ordered.hpp"
 #include "ordered_mult.hpp"
@@ -41,6 +42,19 @@ expand_single(std::tuple<der2::p<Power, der2::d<Order, double_t<N>>>,
                                  der2::p<Powers, der2::d<Orders, Ids>>...>>{};
 }
 
+template <std::size_t Order, class Id1, class Id2, class... CalculationNodes>
+constexpr auto ordered_operators_sum(std::tuple<CalculationNodes...> nodes) {
+    constexpr auto id1_less_than_id2 = static_cast<bool>(
+        get_idx_first2<Id1>(nodes) >= get_idx_first2<Id2>(nodes));
+    if constexpr (id1_less_than_id2) {
+        return std::tuple<der2::p<1, der2::d<Order, Id2>>,
+                          der2::p<1, der2::d<Order, Id1>>>{};
+    } else {
+        return std::tuple<der2::p<1, der2::d<Order, Id1>>,
+                          der2::p<1, der2::d<Order, Id2>>>{};
+    }
+}
+
 template <class Id1, class Id2, class... Ids, std::size_t Order,
           std::size_t... Orders, std::size_t Power, std::size_t... Powers,
           class... CalculationNodes>
@@ -57,16 +71,21 @@ expand_single(std::tuple<der2::p<Power, der2::d<Order, add_t<Id1, Id2>>>,
             std::tuple<der2::p<Power, der2::d<Order, Id1>>,
                        der2::p<Powers, der2::d<Orders, Ids>>...>{});
     } else {
+        constexpr auto ordered_ids =
+            ordered_operators_sum<Order, Id1, Id2>(nodes);
+        // result is (ordered_ids[0] + ordered_ids[1])^Power
+
+        // ERROR! need to expand binomial (d(Id1) + d(Id2)) ^ Power
         auto constexpr ordered_differential_operator_1 = multiply_ordered(
-            std::tuple<der2::p<Power, der2::d<Order, Id1>>>{},
-            std::tuple<der2::p<Powers, der2::d<Orders, Ids>>...>{}, nodes);
+            nodes, std::tuple<der2::p<Power, der2::d<Order, Id1>>>{},
+            std::tuple<der2::p<Powers, der2::d<Orders, Ids>>...>{});
 
         auto constexpr expanded_differential_operator_1 =
             expand_single(ordered_differential_operator_1, nodes);
 
         auto constexpr ordered_differential_operator_2 = multiply_ordered(
-            std::tuple<der2::p<Power, der2::d<Order, Id2>>>{},
-            std::tuple<der2::p<Powers, der2::d<Orders, Ids>>...>{}, nodes);
+            nodes, std::tuple<der2::p<Power, der2::d<Order, Id2>>>{},
+            std::tuple<der2::p<Powers, der2::d<Orders, Ids>>...>{});
 
         auto constexpr expanded_differential_operator_2 =
             expand_single(ordered_differential_operator_2, nodes);
@@ -96,7 +115,57 @@ template <template <class> class Univariate, class Id, class... Ids,
 constexpr auto
 expand_single(std::tuple<der2::p<Power, der2::d<Order, Univariate<Id>>>,
                          der2::p<Powers, der2::d<Orders, Ids>>...> /* id */,
-              std::tuple<CalculationNodes...> /* nodes */) {}
+              std::tuple<CalculationNodes...> /* nodes */) {
+    constexpr auto expansion =
+        integer_partition_univariate_function<Id, Order>();
+}
+
+template <std::size_t Order, class IdFirst, class IdSecond, std::size_t... I>
+constexpr auto generate_operators_mul2(std::index_sequence<I...> /* i */) {
+    return std::make_tuple(
+        std::tuple<der2::p<1, der2::d<Order, IdFirst>>>{},
+        std::tuple<der2::p<1, der2::d<Order - I - 1, IdFirst>>,
+                   der2::p<1, der2::d<I + 1, IdSecond>>>{}...,
+        std::tuple<der2::p<1, der2::d<Order, IdSecond>>>{});
+}
+
+template <std::size_t Order, class Id1, class Id2, class... CalculationNodes>
+constexpr auto ordered_operators_mul(std::tuple<CalculationNodes...> nodes) {
+
+    constexpr auto seq = std::make_integer_sequence<std::size_t, Order - 1>{};
+
+    constexpr auto id1_less_than_id2 = static_cast<bool>(
+        get_idx_first2<Id1>(nodes) >= get_idx_first2<Id2>(nodes));
+    if constexpr (id1_less_than_id2) {
+        return generate_operators_mul2<Order, Id2, Id1>(seq);
+    } else {
+        return generate_operators_mul2<Order, Id1, Id2>(seq);
+    }
+}
+
+template <class Id1, class Id2, class... Ids, std::size_t Order,
+          std::size_t... Orders, std::size_t Power, std::size_t... Powers,
+          class... CalculationNodes>
+constexpr auto
+expand_single(std::tuple<der2::p<Power, der2::d<Order, mul_t<Id1, Id2>>>,
+                         der2::p<Powers, der2::d<Orders, Ids>>...> /* id */,
+              std::tuple<CalculationNodes...> nodes) {
+    if constexpr (is_constant_class_v<Id1>) {
+        return expand_single(
+            std::tuple<der2::p<Power, der2::d<Order, Id2>>,
+                       der2::p<Powers, der2::d<Orders, Ids>>...>{});
+    } else if constexpr (is_constant_class_v<Id2>) {
+        return expand_single(
+            std::tuple<der2::p<Power, der2::d<Order, Id1>>,
+                       der2::p<Powers, der2::d<Orders, Ids>>...>{});
+    } else {
+        constexpr auto ordered_ids =
+            ordered_operators_mul<Order, Id1, Id2>(nodes);
+        // ordered_ids contains tuple(d^Order-I(Id1)*d^I(Id2))
+
+        return std::tuple<>{};
+    }
+}
 
 } // namespace detail
 
--- public3/differential_operator/ordered_mult.hpp
+++ public3/differential_operator/ordered_mult.hpp
@@ -14,7 +14,7 @@ namespace detail {
 template <class Id1, class... Ids1, std::size_t Order1, std::size_t... Orders1,
           std::size_t Power1, std::size_t... Powers1, class Id2, class... Ids2,
           std::size_t Order2, std::size_t... Orders2, std::size_t Power2,
-          std::size_t... Powers2, class... Out, class... CalculationNodes>
+          std::size_t... Powers2, class... Out, class Nodes>
 constexpr auto
 multiply_ordered_compare(std::tuple<der2::p<Power1, der2::d<Order1, Id1>>,
                                     der2::p<Powers1, der2::d<Orders1, Ids1>>...>
@@ -22,20 +22,14 @@ multiply_ordered_compare(std::tuple<der2::p<Power1, der2::d<Order1, Id1>>,
                          std::tuple<der2::p<Power2, der2::d<Order2, Id2>>,
                                     der2::p<Powers2, der2::d<Orders2, Ids2>>...>
                              in2,
-                         std::tuple<Out...> out,
-                         std::tuple<CalculationNodes...> nodes);
+                         std::tuple<Out...> out, Nodes nodes);
 
-template <class... Ids1, std::size_t... Orders1, std::size_t... Powers1,
-          class... Ids2, std::size_t... Orders2, std::size_t... Powers2,
-          class... Out, class... CalculationNodes>
-constexpr auto multiply_ordered_empty(
-    std::tuple<der2::p<Powers1, der2::d<Orders1, Ids1>>...> in1,
-    std::tuple<der2::p<Powers2, der2::d<Orders2, Ids2>>...> in2,
-    std::tuple<Out...> out, std::tuple<CalculationNodes...> nodes) {
-
-    if constexpr (sizeof...(Ids2) == 0) {
+template <class DerOp1, class DerOp2, class Out, class Nodes>
+constexpr auto multiply_ordered_empty(DerOp1 in1, DerOp2 in2, Out out,
+                                      Nodes nodes) {
+    if constexpr (std::tuple_size_v<DerOp2> == 0) {
         return std::tuple_cat(out, in1);
-    } else if constexpr (sizeof...(Ids1) == 0) {
+    } else if constexpr (std::tuple_size_v<DerOp1> == 0) {
         return std::tuple_cat(out, in2);
     } else {
         return multiply_ordered_compare(in1, in2, out, nodes);
@@ -45,7 +39,7 @@ constexpr auto multiply_ordered_empty(
 template <class Id1, class... Ids1, std::size_t Order1, std::size_t... Orders1,
           std::size_t Power1, std::size_t... Powers1, class Id2, class... Ids2,
           std::size_t Order2, std::size_t... Orders2, std::size_t Power2,
-          std::size_t... Powers2, class... Out, class... CalculationNodes>
+          std::size_t... Powers2, class... Out, class Nodes>
 constexpr auto
 multiply_ordered_compare(std::tuple<der2::p<Power1, der2::d<Order1, Id1>>,
                                     der2::p<Powers1, der2::d<Orders1, Ids1>>...>
@@ -53,8 +47,7 @@ multiply_ordered_compare(std::tuple<der2::p<Power1, der2::d<Order1, Id1>>,
                          std::tuple<der2::p<Power2, der2::d<Order2, Id2>>,
                                     der2::p<Powers2, der2::d<Orders2, Ids2>>...>
                              in2,
-                         std::tuple<Out...> /* out */,
-                         std::tuple<CalculationNodes...> nodes) {
+                         std::tuple<Out...> /* out */, Nodes nodes) {
 
     if constexpr (less_than(der2::p<Power1, der2::d<Order1, Id1>>{},
                             der2::p<Power2, der2::d<Order2, Id2>>{}, nodes)) {
@@ -86,16 +79,26 @@ multiply_ordered_compare(std::tuple<der2::p<Power1, der2::d<Order1, Id1>>,
 
 } // namespace detail
 
-template <class... Ids1, std::size_t... Orders1, std::size_t... Powers1,
-          class... Ids2, std::size_t... Orders2, std::size_t... Powers2,
-          class... CalculationNodes>
-constexpr auto
-multiply_ordered(std::tuple<der2::p<Powers1, der2::d<Orders1, Ids1>>...> in1,
-                 std::tuple<der2::p<Powers2, der2::d<Orders2, Ids2>>...> in2,
-                 std::tuple<CalculationNodes...> nodes) {
+template <class Nodes, class DerOp>
+constexpr auto multiply_ordered(Nodes nodes, DerOp in1) {
+    static_assert(is_ordered(in1, nodes));
+    return in1;
+};
+
+template <class Nodes, class DerOp1, class DerOp2>
+constexpr auto multiply_ordered(Nodes nodes, DerOp1 in1, DerOp2 in2) {
     static_assert(is_ordered(in1, nodes));
     static_assert(is_ordered(in2, nodes));
     return detail::multiply_ordered_empty(in1, in2, std::tuple<>{}, nodes);
+};
+
+template <class Nodes, class DerOp1, class DerOp2, class... DerOps>
+constexpr auto multiply_ordered(Nodes nodes, DerOp1 in1, DerOp2 in2,
+                                DerOps... ins) {
+    static_assert(is_ordered(in1, nodes));
+    static_assert(is_ordered(in2, nodes));
+    constexpr auto mult = multiply_ordered(nodes, in1, in2);
+    return multiply_ordered(nodes, mult, ins...);
 };
 
 } // namespace adhoc3
