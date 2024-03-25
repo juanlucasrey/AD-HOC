489f5c640c48007a1b6052374d77162163990994
lrey4 <lrey4@bloomberg.net>
2024-03-25 14:08:04 +0000

    nodes first

--- public3/differential_operator/ordered_mult.hpp
+++ public3/differential_operator/ordered_mult.hpp
@@ -11,69 +11,71 @@ namespace adhoc3 {
 
 namespace detail {
 
-template <class Id1, class... Ids1, std::size_t Order1, std::size_t... Orders1,
-          std::size_t Power1, std::size_t... Powers1, class Id2, class... Ids2,
-          std::size_t Order2, std::size_t... Orders2, std::size_t Power2,
-          std::size_t... Powers2, class... Out, class Nodes>
+template <class Nodes, class Id1, class... Ids1, std::size_t Order1,
+          std::size_t... Orders1, std::size_t Power1, std::size_t... Powers1,
+          class Id2, class... Ids2, std::size_t Order2, std::size_t... Orders2,
+          std::size_t Power2, std::size_t... Powers2, class... Out>
 constexpr auto
-multiply_ordered_compare(std::tuple<der2::p<Power1, der2::d<Order1, Id1>>,
+multiply_ordered_compare(Nodes nodes,
+                         std::tuple<der2::p<Power1, der2::d<Order1, Id1>>,
                                     der2::p<Powers1, der2::d<Orders1, Ids1>>...>
                              in1,
                          std::tuple<der2::p<Power2, der2::d<Order2, Id2>>,
                                     der2::p<Powers2, der2::d<Orders2, Ids2>>...>
                              in2,
-                         std::tuple<Out...> out, Nodes nodes);
+                         std::tuple<Out...> out);
 
-template <class DerOp1, class DerOp2, class Out, class Nodes>
-constexpr auto multiply_ordered_empty(DerOp1 in1, DerOp2 in2, Out out,
-                                      Nodes nodes) {
+template <class Nodes, class DerOp1, class DerOp2, class Out>
+constexpr auto multiply_ordered_empty(Nodes nodes, DerOp1 in1, DerOp2 in2,
+                                      Out out) {
     if constexpr (std::tuple_size_v<DerOp2> == 0) {
         return std::tuple_cat(out, in1);
     } else if constexpr (std::tuple_size_v<DerOp1> == 0) {
         return std::tuple_cat(out, in2);
     } else {
-        return multiply_ordered_compare(in1, in2, out, nodes);
+        return multiply_ordered_compare(nodes, in1, in2, out);
     }
 }
 
-template <class Id1, class... Ids1, std::size_t Order1, std::size_t... Orders1,
-          std::size_t Power1, std::size_t... Powers1, class Id2, class... Ids2,
-          std::size_t Order2, std::size_t... Orders2, std::size_t Power2,
-          std::size_t... Powers2, class... Out, class Nodes>
+template <class Nodes, class Id1, class... Ids1, std::size_t Order1,
+          std::size_t... Orders1, std::size_t Power1, std::size_t... Powers1,
+          class Id2, class... Ids2, std::size_t Order2, std::size_t... Orders2,
+          std::size_t Power2, std::size_t... Powers2, class... Out>
 constexpr auto
-multiply_ordered_compare(std::tuple<der2::p<Power1, der2::d<Order1, Id1>>,
+multiply_ordered_compare(Nodes nodes,
+                         std::tuple<der2::p<Power1, der2::d<Order1, Id1>>,
                                     der2::p<Powers1, der2::d<Orders1, Ids1>>...>
                              in1,
                          std::tuple<der2::p<Power2, der2::d<Order2, Id2>>,
                                     der2::p<Powers2, der2::d<Orders2, Ids2>>...>
                              in2,
-                         std::tuple<Out...> /* out */, Nodes nodes) {
+                         std::tuple<Out...> /* out */) {
 
     if constexpr (less_than(nodes, der2::p<Power1, der2::d<Order1, Id1>>{},
                             der2::p<Power2, der2::d<Order2, Id2>>{})) {
 
         return multiply_ordered_empty(
-            in1, std::tuple<der2::p<Powers2, der2::d<Orders2, Ids2>>...>{},
-            std::tuple<Out..., der2::p<Power2, der2::d<Order2, Id2>>>{}, nodes);
+            nodes, in1,
+            std::tuple<der2::p<Powers2, der2::d<Orders2, Ids2>>...>{},
+            std::tuple<Out..., der2::p<Power2, der2::d<Order2, Id2>>>{});
 
     } else if constexpr (less_than(nodes,
                                    der2::p<Power2, der2::d<Order2, Id2>>{},
                                    der2::p<Power1, der2::d<Order1, Id1>>{})) {
 
         return multiply_ordered_empty(
-            std::tuple<der2::p<Powers1, der2::d<Orders1, Ids1>>...>{}, in2,
-            std::tuple<Out..., der2::p<Power1, der2::d<Order1, Id1>>>{}, nodes);
+            nodes, std::tuple<der2::p<Powers1, der2::d<Orders1, Ids1>>...>{},
+            in2, std::tuple<Out..., der2::p<Power1, der2::d<Order1, Id1>>>{});
 
     } else {
 
         static_assert(
             std::is_same_v<der2::d<Order1, Id1>, der2::d<Order2, Id2>>);
         return multiply_ordered_empty(
-            std::tuple<der2::p<Powers1, der2::d<Orders1, Ids1>>...>{},
+            nodes, std::tuple<der2::p<Powers1, der2::d<Orders1, Ids1>>...>{},
             std::tuple<der2::p<Powers2, der2::d<Orders2, Ids2>>...>{},
             std::tuple<Out...,
-                       der2::p<Power1 + Power2, der2::d<Order1, Id1>>>{},
-            nodes);
+                       der2::p<Power1 + Power2, der2::d<Order1, Id1>>>{});
     }
 }
 
@@ -93,7 +95,7 @@ template <class Nodes, class DerOp1, class DerOp2>
 constexpr auto multiply_ordered(Nodes nodes, DerOp1 in1, DerOp2 in2) {
     static_assert(is_ordered(nodes, in1));
     static_assert(is_ordered(nodes, in2));
-    return detail::multiply_ordered_empty(in1, in2, std::tuple<>{}, nodes);
+    return detail::multiply_ordered_empty(nodes, in1, in2, std::tuple<>{});
 };
 
 template <class Nodes, class DerOp1, class DerOp2, class... DerOps>
