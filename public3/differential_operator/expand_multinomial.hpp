2dc0af4f5daa14057eb240b6451a0420a23c15bc
lrey4 <lrey4@bloomberg.net>
2024-03-30 23:08:12 +0700

    expand multinomial

--- public3/differential_operator/expand_multinomial.hpp
+++ public3/differential_operator/expand_multinomial.hpp
@@ -3,6 +3,7 @@
 
 #include "../partition/combinations.hpp"
 #include "../partition/multinomial_coefficient_index_sequence.hpp"
+#include "is_ordered.hpp"
 #include "ordered_mult.hpp"
 
 #include <cstddef>
@@ -13,26 +14,17 @@ namespace adhoc3 {
 
 namespace detail {
 
-template <class Nodes, class... Ids, std::size_t... I>
-constexpr auto expand_single(Nodes nodes, std::tuple<Ids...> /* in */,
-                             std::index_sequence<I...> /* i */) {
-    static_assert(sizeof...(Ids) == sizeof...(I));
-    return multiply_ordered(nodes, pow<I>(Ids{})...);
-}
-
 template <std::size_t Idx, std::size_t End, class Nodes, class... Ids,
-          class Prev, class... Out>
-constexpr auto expand_multinomial(Nodes nodes, std::tuple<Ids...> in, Prev prev,
-                                  std::tuple<Out...> out) {
-    auto constexpr curent = expand_single(nodes, in, prev);
+          std::size_t... I, class... Out>
+constexpr auto expand_multinomial_aux(Nodes nodes, std::tuple<Ids...> in,
+                                      std::index_sequence<I...> prev,
+                                      std::tuple<Out...> out) {
+    auto constexpr curent = multiply_ordered(nodes, pow<I>(Ids{})...);
     if constexpr (Idx == End) {
         return std::tuple_cat(out, std::make_tuple(curent));
-        // return std::tuple<Out..., Prev>{};
     } else {
         auto constexpr next = NextMultinomialIS(prev);
-        // return expand_multinomial<Idx + 1, End>(nodes, in, next,
-        //                                         std::tuple<Out..., Prev>{});
-        return expand_multinomial<Idx + 1, End>(
+        return expand_multinomial_aux<Idx + 1, End>(
             nodes, in, next, std::tuple_cat(out, std::make_tuple(curent)));
     }
 };
@@ -41,8 +33,10 @@ constexpr auto expand_multinomial(Nodes nodes, std::tuple<Ids...> in, Prev prev,
 
 template <std::size_t Power, class Nodes, class... Ids>
 constexpr auto expand_multinomial(Nodes nodes, std::tuple<Ids...> in) {
+    static_assert(is_ordered(nodes, in));
     constexpr auto first = FirstMultinomialIS<sizeof...(Ids), Power>();
-    return detail::expand_multinomial<1, combinations(sizeof...(Ids), Power)>(
+    return detail::expand_multinomial_aux<1,
+                                          combinations(sizeof...(Ids), Power)>(
         nodes, in, first, std::tuple<>{});
 }
 
---
+++ tests3/differential_operator/expand_multinomial.t.cpp
@@ -0,0 +1,36 @@
+#include <differential_operator/expand_multinomial.hpp>
+
+#include <init.hpp>
+#include <tape2.hpp>
+
+#include "../type_name.hpp"
+
+#include <gtest/gtest.h>
+
+namespace adhoc3 {
+
+TEST(DifferentialOperator, ExpandMultinomial) {
+    auto [x1, x2] = Init<2>();
+    auto res = x1 * x2;
+    CalcTree t(res);
+    // we create some calculation order type
+    decltype(t)::ValuesTupleInverse co;
+
+    auto d1 = d(x1);
+    auto d2 = d<2>(res);
+    auto d3 = d<3>(x2);
+
+    constexpr auto deriv_tuple =
+        std::tuple<decltype(d2), decltype(d1), decltype(d3)>{};
+    constexpr auto result = expand_multinomial<3>(co, deriv_tuple);
+
+    constexpr auto result2 =
+        std::tuple<decltype(pow<3>(d2)), decltype(pow<2>(d2) * d1),
+                   decltype(pow<2>(d2) * d3), decltype(d2 * pow<2>(d1)),
+                   decltype(d2 * d1 * d3), decltype(d2 * pow<2>(d3)),
+                   decltype(pow<3>(d1)), decltype(pow<2>(d1) * d3),
+                   decltype(d1 * pow<2>(d3)), decltype(pow<3>(d3))>{};
+    static_assert(std::is_same_v<decltype(result), decltype(result2)>);
+}
+
+} // namespace adhoc3
