b4d5edc50dd51a0bfd5857ee0e570def598d0885
lrey4 <lrey4@bloomberg.net>
2024-04-12 20:55:21 +0700

    tape buffer size simplification

--- public3/tape_buffer_size.hpp
+++ public3/tape_buffer_size.hpp
@@ -8,8 +8,7 @@ namespace adhoc3 {
 
 namespace detail {
 
-template <std::size_t CurrentWidth, std::size_t MaxWidth, class Nodes,
-          class DersIn, class DersActive>
+template <std::size_t MaxWidth, class Nodes, class DersIn, class DersActive>
 constexpr auto
 tape_buffer_size_aux(Nodes /* nodes */, std::tuple<> /* expandedtree */,
                      DersIn /* dersin */, DersActive /* dersactive */)
@@ -31,8 +30,8 @@ constexpr auto conditional_node(DerivativeNodes dernodes, DersIn dersin,
     }
 }
 
-template <std::size_t CurrentWidth, std::size_t MaxWidth, class Nodes,
-          class FirstNode, class... RestNodes, class DersIn, class DersActive>
+template <std::size_t MaxWidth, class Nodes, class FirstNode,
+          class... RestNodes, class DersIn, class DersActive>
 constexpr auto
 tape_buffer_size_aux(Nodes nodes,
                      std::tuple<FirstNode, RestNodes...> /* expandedtree */,
@@ -51,20 +50,14 @@ tape_buffer_size_aux(Nodes nodes,
     constexpr auto new_dersactive =
         std::tuple_cat(dersactive_current_removed, result_filtered);
 
-    constexpr bool is_this_node_input = contains(dersin, FirstNode{});
-
     constexpr std::size_t new_current_widt =
-        CurrentWidth - !is_this_node_input +
-        std::tuple_size_v<decltype(result_filtered)>;
-
-    static_assert(new_current_widt ==
-                  std::tuple_size_v<decltype(new_dersactive)>);
+        std::tuple_size_v<decltype(new_dersactive)>;
 
     constexpr std::size_t new_max_widt =
         new_current_widt > MaxWidth ? new_current_widt : MaxWidth;
 
-    return tape_buffer_size_aux<new_current_widt, new_max_widt>(
-        nodes, std::tuple<RestNodes...>{}, dersin, new_dersactive);
+    return tape_buffer_size_aux<new_max_widt>(nodes, std::tuple<RestNodes...>{},
+                                              dersin, new_dersactive);
 }
 
 } // namespace detail
@@ -72,8 +65,8 @@ tape_buffer_size_aux(Nodes nodes,
 template <class Nodes, class ExpandedTree, class DersIn>
 constexpr auto tape_buffer_size(Nodes nodes, ExpandedTree expandedtree,
                                 DersIn dersin) -> std::size_t {
-    return detail::tape_buffer_size_aux<0U, 0U>(nodes, expandedtree, dersin,
-                                                std::tuple<>{});
+    return detail::tape_buffer_size_aux<0U>(nodes, expandedtree, dersin,
+                                            std::tuple<>{});
 }
 
 } // namespace adhoc3
