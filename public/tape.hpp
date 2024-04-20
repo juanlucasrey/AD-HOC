ee336136fc198a65f520f827f2ef43949d698390
lrey4 <lrey4@bloomberg.net>
2024-04-20 11:16:19 +0100

    license

--- public/tape.hpp
+++ public/tape.hpp
@@ -1,10 +1,30 @@
+/*
+ * AD-HOC, Automatic Differentiation for High Order Calculations
+ *
+ * This file is part of the AD-HOC distribution
+ * (https://github.com/juanlucasrey/adhoc2).
+ * Copyright (c) 2024 Juan Lucas Rey
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef ADHOC_TAPE_HPP
 #define ADHOC_TAPE_HPP
 
 #include "calc_order.hpp"
 #include "leafs.hpp"
 #include "tape_size_bwd.hpp"
-#include "tape_size_fwd.hpp"
+// #include "tape_size_fwd.hpp"
 #include "tuple_utils.hpp"
 #include "utils.hpp"
 
--- public/tape_size_fwd.hpp
+++ public/tape_size_fwd.hpp
@@ -1,3 +1,23 @@
+/*
+ * AD-HOC, Automatic Differentiation for High Order Calculations
+ *
+ * This file is part of the AD-HOC distribution
+ * (https://github.com/juanlucasrey/adhoc2).
+ * Copyright (c) 2024 Juan Lucas Rey
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef ADHOC_TAPE_SIZE_FWD_HPP
 #define ADHOC_TAPE_SIZE_FWD_HPP
 
@@ -12,25 +32,26 @@ namespace adhoc {
 
 namespace detail {
 
-template <class... Roots> struct tape_size_fwd_aux_t {
-    template <class... Leafs, class... Nodes>
-    constexpr static auto call(std::tuple<Leafs...> /* in1 */,
-                               std::tuple<Nodes...> /* in2 */) {
-        return std::tuple_cat(
-            std::conditional_t<has_type<Nodes, Leafs..., Roots...>(),
-                               std::tuple<>, std::tuple<Nodes>>{}...);
-    }
-};
+// template <class... Roots> struct tape_size_fwd_aux_t {
+//     template <class... Leafs, class... Nodes>
+//     constexpr static auto call(std::tuple<Leafs...> /* in1 */,
+//                                std::tuple<Nodes...> /* in2 */) {
+//         return std::tuple_cat(
+//             std::conditional_t<has_type<Nodes, Leafs..., Roots...>(),
+//                                std::tuple<>, std::tuple<Nodes>>{}...);
+//     }
+// };
 
 } // namespace detail
 
-template <class... Roots> constexpr auto tape_size_fwd_t(Roots... /* in */) {
-    auto constexpr leafs = detail::leafs_t<Roots...>::template call();
-    auto constexpr nodes =
-        detail::calc_order_aux_t<true, Roots...>::template call();
+// template <class... Roots> constexpr auto tape_size_fwd_t(Roots... /* in */) {
+//     auto constexpr leafs = detail::leafs_t<Roots...>::template call();
+//     auto constexpr nodes =
+//         detail::calc_order_aux_t<true, Roots...>::template call();
 
-    return detail::tape_size_fwd_aux_t<Roots...>::template call(leafs, nodes);
-}
+//     return detail::tape_size_fwd_aux_t<Roots...>::template call(leafs,
+//     nodes);
+// }
 
 } // namespace adhoc
 
