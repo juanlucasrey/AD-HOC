09229d0726085092654aa6318dc154d26757ab24
juanlucasrey <juanlucasrey@icloud.com>
2024-10-12 11:23:06 +0100

    separate finite differences calculator

---
+++ tests4/finite_differences.cpp
@@ -0,0 +1,70 @@
+#include "finite_differences.hpp"
+
+#include <array>
+#include <cmath>
+#include <functional>
+#include <numeric>
+
+namespace adhoc4 {
+
+std::array<double, 6> finite_differences(double x, double epsilon,
+                                         std::function<double(double)> func) {
+    std::array<double, 11> fvals{
+        func(x - 5. * epsilon), func(x - 4. * epsilon), func(x - 3. * epsilon),
+        func(x - 2. * epsilon), func(x - 1. * epsilon), func(x),
+        func(x + 1. * epsilon), func(x + 2. * epsilon), func(x + 3. * epsilon),
+        func(x + 4. * epsilon), func(x + 5. * epsilon)};
+
+    constexpr std::array<double, 11> weights1 = {
+        0.,      1. / 280., -4. / 105., 1. / 5.,   -4. / 5., 0.,
+        4. / 5., -1. / 5.,  4. / 105.,  -1. / 280, 0.};
+
+    constexpr std::array<double, 11> weights2 = {
+        0.,      -1. / 560., 8. / 315., -1. / 5.,   8. / 5., -205. / 72.,
+        8. / 5., -1. / 5.,   8. / 315., -1. / 560., 0.};
+
+    constexpr std::array<double, 11> weights3 = {
+        0.,         -7. / 240.,  3. / 10.,  -169. / 120., 61. / 30., 0.,
+        -61. / 30., 169. / 120., -3. / 10., 7. / 240.,    0.};
+
+    constexpr std::array<double, 11> weights4 = {
+        0.,          7. / 240.,  -2. / 5., 169. / 60., -122. / 15., 91. / 8.,
+        -122. / 15., 169. / 60., -2. / 5., 7. / 240.,  0.};
+
+    constexpr std::array<double, 11> weights5 = {
+        -13. / 288., 19. / 36., -87. / 32., 13. / 2.,   -323. / 48., 0.,
+        323. / 48.,  -13. / 2., 87. / 32.,  -19. / 36., 13. / 288.};
+
+    constexpr std::array<double, 11> weights6 = {
+        13. / 240., -19. / 24., 87. / 16., -39. / 2.,  323. / 8., -1023. / 20.,
+        323. / 8.,  -39. / 2.,  87. / 16., -19. / 24., 13. / 240.};
+
+    std::array<double, 6> result{0., 0., 0., 0., 0., 0.};
+    result[0] = std::transform_reduce(fvals.begin(), fvals.end(),
+                                      weights1.begin(), 0.) /
+                epsilon;
+
+    result[1] = std::transform_reduce(fvals.begin(), fvals.end(),
+                                      weights2.begin(), 0.) /
+                std::pow(epsilon, 2);
+
+    result[2] = std::transform_reduce(fvals.begin(), fvals.end(),
+                                      weights3.begin(), 0.) /
+                std::pow(epsilon, 3);
+
+    result[3] = std::transform_reduce(fvals.begin(), fvals.end(),
+                                      weights4.begin(), 0.) /
+                std::pow(epsilon, 4);
+
+    result[4] = std::transform_reduce(fvals.begin(), fvals.end(),
+                                      weights5.begin(), 0.) /
+                std::pow(epsilon, 5);
+
+    result[5] = std::transform_reduce(fvals.begin(), fvals.end(),
+                                      weights6.begin(), 0.) /
+                std::pow(epsilon, 6);
+
+    return result;
+}
+
+} // namespace adhoc4
--- tests4/univariate_vs_fd.t.cpp
+++ tests4/univariate_vs_fd.t.cpp
@@ -1,75 +1,12 @@
 #include <adhoc.hpp>
 #include <base.hpp>
 
+#include "finite_differences.hpp"
 #include <gtest/gtest.h>
 
 #include <cmath>
-#include <numeric>
 
 namespace adhoc4 {
-
-namespace {
-std::array<double, 6> finite_differences(double x, double epsilon,
-                                         std::function<double(double)> func) {
-    std::vector<double> fvals{
-        func(x - 5. * epsilon), func(x - 4. * epsilon), func(x - 3. * epsilon),
-        func(x - 2. * epsilon), func(x - 1. * epsilon), func(x),
-        func(x + 1. * epsilon), func(x + 2. * epsilon), func(x + 3. * epsilon),
-        func(x + 4. * epsilon), func(x + 5. * epsilon)};
-
-    std::vector<double> weights1 = {0.,        1. / 280., -4. / 105., 1. / 5.,
-                                    -4. / 5.,  0.,        4. / 5.,    -1. / 5.,
-                                    4. / 105., -1. / 280, 0.};
-
-    std::vector<double> weights2 = {0.,        -1. / 560.,  8. / 315., -1. / 5.,
-                                    8. / 5.,   -205. / 72., 8. / 5.,   -1. / 5.,
-                                    8. / 315., -1. / 560.,  0.};
-
-    std::vector<double> weights3 = {
-        0.,         -7. / 240.,  3. / 10.,  -169. / 120., 61. / 30., 0.,
-        -61. / 30., 169. / 120., -3. / 10., 7. / 240.,    0.};
-
-    std::vector<double> weights4 = {
-        0.,          7. / 240.,  -2. / 5., 169. / 60., -122. / 15., 91. / 8.,
-        -122. / 15., 169. / 60., -2. / 5., 7. / 240.,  0.};
-
-    std::vector<double> weights5 = {
-        -13. / 288., 19. / 36., -87. / 32., 13. / 2.,   -323. / 48., 0.,
-        323. / 48.,  -13. / 2., 87. / 32.,  -19. / 36., 13. / 288.};
-
-    std::vector<double> weights6 = {
-        13. / 240., -19. / 24., 87. / 16., -39. / 2.,  323. / 8., -1023. / 20.,
-        323. / 8.,  -39. / 2.,  87. / 16., -19. / 24., 13. / 240.};
-
-    std::array<double, 6> result;
-    result.fill(0);
-    result[0] = std::transform_reduce(fvals.begin(), fvals.end(),
-                                      weights1.begin(), 0.) /
-                epsilon;
-
-    result[1] = std::transform_reduce(fvals.begin(), fvals.end(),
-                                      weights2.begin(), 0.) /
-                std::pow(epsilon, 2);
-
-    result[2] = std::transform_reduce(fvals.begin(), fvals.end(),
-                                      weights3.begin(), 0.) /
-                std::pow(epsilon, 3);
-
-    result[3] = std::transform_reduce(fvals.begin(), fvals.end(),
-                                      weights4.begin(), 0.) /
-                std::pow(epsilon, 4);
-
-    result[4] = std::transform_reduce(fvals.begin(), fvals.end(),
-                                      weights5.begin(), 0.) /
-                std::pow(epsilon, 5);
-
-    result[5] = std::transform_reduce(fvals.begin(), fvals.end(),
-                                      weights6.begin(), 0.) /
-                std::pow(epsilon, 6);
-
-    return result;
-}
-} // namespace
 
 TEST(UnivariateFunctions, Inverse) {
     std::array<double, 10> results1;
