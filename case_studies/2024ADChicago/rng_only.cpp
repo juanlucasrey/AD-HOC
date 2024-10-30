d451e62369199cb66baa83b82d1297dcce8f6181
juanlucasrey <juanlucasrey@icloud.com>
2024-10-30 07:09:29 +0000

    rng only time

---
+++ case_studies/2024ADChicago/rng_only.cpp
@@ -0,0 +1,44 @@
+#include "black_scholes.hpp"
+
+#include <array>
+#include <chrono>
+#include <iostream>
+#include <random>
+
+int main() {
+    std::mt19937 generator(123);
+    std::uniform_real_distribution<double> stock_distr(90, 110.0),
+        vol_distr(0.05, 0.3), time_distr(0.5, 1.5);
+
+    std::size_t iters = 1;
+    if (auto env_p = std::getenv("ITERATIONS")) {
+        iters = std::stoul(env_p);
+    }
+
+    std::array<double, 1> results_average{};
+
+    auto time1 = std::chrono::high_resolution_clock::now();
+
+    for (std::size_t j = 0; j < iters; ++j) {
+        results_average[0] += stock_distr(generator);
+        results_average[0] += stock_distr(generator);
+        results_average[0] += vol_distr(generator);
+        results_average[0] += time_distr(generator);
+    }
+
+    auto time2 = std::chrono::high_resolution_clock::now();
+    auto time =
+        std::chrono::duration_cast<std::chrono::milliseconds>(time2 - time1)
+            .count();
+
+    std::cout << "iterations: " << iters << std::endl;
+    std::cout << "rng only time (ms): " << time << std::endl;
+
+    std::cout.precision(std::numeric_limits<double>::max_digits10);
+    for (std::size_t i = 0; i < results_average.size(); ++i) {
+        results_average[i] /= static_cast<double>(iters);
+        std::cout << results_average[i] << std::endl;
+    }
+
+    return 0;
+}
