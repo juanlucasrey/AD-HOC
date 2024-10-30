363a4d356981f4d2ff76affd48cc92209525309b
Tim Siebert <tim.siebert96@outlook.com>
2024-10-30 20:46:28 +0100

    initial hess computation with fov and rev

---
+++ case_studies/2024ADChicago/adolc_order2_alt.cpp
@@ -0,0 +1,85 @@
+#include <adolc/adolc.h>
+
+#include "black_scholes_no_erfc.hpp"
+
+#include <array>
+#include <chrono>
+#include <iostream>
+#include <random>
+
+int factorial(int n) { return (n == 1 || n == 0) ? 1 : factorial(n - 1) * n; }
+
+int main() {
+    std::mt19937 generator(123);
+    std::uniform_real_distribution<double> stock_distr(90, 110.0),
+        vol_distr(0.05, 0.3), time_distr(0.5, 1.5);
+
+    std::size_t iters = 1000;
+    if (auto env_p = std::getenv("ITERATIONS")) {
+        iters = std::stoul(env_p);
+    }
+
+    std::array<double, 15> results_average{};
+
+    auto time1 = std::chrono::high_resolution_clock::now();
+
+    int n = 4;
+    int m = 1;
+
+    double *xp = new double[n];
+    double *yp = new double[m];
+
+    auto x = new adouble[n];
+    auto y = new adouble[m];
+
+    int tag = 0;
+    trace_on(tag);
+
+    for (std::size_t i = 0; i < n; ++i) {
+        x[i] <<= xp[i];
+    }
+    y[0] = call_price(x[0], x[1], x[2], x[3]);
+    y[0] >>= yp[0];
+
+    trace_off();
+    double *x_tangent = myalloc1(n);
+    double *y_tangent = myalloc1(n);
+    double *weights = myalloc1(m);
+    weights[0] = 1.0;
+    double **res = myalloc2(n, 2);
+    for (int i = 0; i < n; i++)
+        x_tangent[i] = 0;
+
+    for (std::size_t j = 0; j < iters; ++j) {
+        xp[0] = stock_distr(generator);
+        xp[1] = stock_distr(generator);
+        xp[2] = vol_distr(generator);
+        xp[3] = time_distr(generator);
+
+        for (int i = 0; i < n; i++) {
+            x_tangent[i] = 1.0;
+            fos_forward(tag, m, n, 2, xp, x_tangent, yp, y_tangent);
+            results_average[i + 1] += y_tangent[i];
+            hos_reverse(tag, m, n, 1, weights, res);
+            results_average[i + 4] += res[i][i];
+            x_tangent[i] = 0.0;
+        }
+        results_average[0] = yp[0];
+    }
+
+    auto time2 = std::chrono::high_resolution_clock::now();
+    auto time =
+        std::chrono::duration_cast<std::chrono::milliseconds>(time2 - time1)
+            .count();
+
+    std::cout.precision(std::numeric_limits<double>::max_digits10);
+    for (std::size_t i = 0; i < results_average.size(); ++i) {
+        results_average[i] /= static_cast<double>(iters);
+        std::cout << results_average[i] << std::endl;
+    }
+
+    std::cout << "iterations: " << iters << std::endl;
+    std::cout << "ADOL-C order 2 time (ms): " << time << std::endl;
+
+    return 0;
+}
