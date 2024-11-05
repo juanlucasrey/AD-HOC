e6536d4696983dc654b2268be19604444af6381d
juanlucasrey <juanlucasrey@icloud.com>
2024-11-05 21:23:11 +0000

    static tests with ctest

---
+++ test/differential_operator2.t.cpp
@@ -0,0 +1,15 @@
+#include <adhoc.hpp>
+#include <differential_operator.hpp>
+
+int main() {
+    ADHOC(var);
+    ADHOC(var2);
+    auto res = d(var) * d(var2 * var);
+
+    static_assert(std::same_as<decltype(res * res),
+                               decltype(d<2>(var) * d<2>(var2 * var))>);
+
+    constexpr double minus_one_over_root_two =
+        -1.0 / std::numbers::sqrt2_v<double>;
+    return 0;
+}
