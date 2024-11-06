81bd838176b4ccb5f64188096342af0730513f9d
juanlucasrey <juanlucasrey@icloud.com>
2024-11-06 18:01:05 +0000

    moving black scholes 2 to ctest

--- test/black_scholes2.cpp
+++ test/black_scholes2.cpp
@@ -4,23 +4,22 @@
 #include <differential_operator.hpp>
 
 #include "call_price.hpp"
-#include <gtest/gtest.h>
+#include "test_tools.hpp"
 
-namespace adhoc4 {
-
-TEST(BlackScholes, Order2) {
+int main() {
+    using namespace adhoc4;
     ADHOC(S);
     ADHOC(K);
     ADHOC(v);
     ADHOC(T);
 
-    auto res = call_price2(S, K, v, T);
+    auto res = call_price(S, K, v, T);
 
     CalcTree ct(res);
-    ct.set(S) = 1.01;
-    ct.set(K) = 1.02;
-    ct.set(v) = 0.15;
-    ct.set(T) = 0.5;
+    ct.set(S) = 104.;
+    ct.set(K) = 98.;
+    ct.set(v) = 0.22;
+    ct.set(T) = 1.25;
     ct.evaluate();
 
     // order 1
@@ -31,39 +30,39 @@ TEST(BlackScholes, Order2) {
 
     // order 2
     auto dSS = d<2>(S);
-    auto dKK = d<2>(K);
-    auto dvv = d<2>(v);
-    auto dTT = d<2>(T);
     auto dSK = d(S) * d(K);
     auto dSv = d(S) * d(v);
     auto dST = d(S) * d(T);
+    auto dKK = d<2>(K);
     auto dKv = d(K) * d(v);
     auto dKT = d(K) * d(T);
+    auto dvv = d<2>(v);
     auto dvT = d(v) * d(T);
+    auto dTT = d<2>(T);
 
     auto dres = d(res);
 
-    BackPropagator t(dS, dK, dv, dT, dSS, dKK, dvv, dTT, dSK, dSv, dST, dKv,
-                     dKT, dvT, dres);
+    BackPropagator bp(dS, dK, dv, dT, dSS, dSK, dSv, dST, dKK, dKv, dKT, dvv,
+                      dvT, dTT, dres);
 
-    t.set(dres) = 1.;
-    t.backpropagate(ct);
+    bp.set(dres) = 1.;
+    bp.backpropagate(ct);
 
-    EXPECT_NEAR(ct.get(res), 1.02586812693369, 1e-14);
-    EXPECT_NEAR(t.get(dS), 8.03256922212305, 1e-14);
-    EXPECT_NEAR(t.get(dK), -6.94806547785353, 1e-14);
-    EXPECT_NEAR(t.get(dv), 1.07376087149468, 1e-14);
-    EXPECT_NEAR(t.get(dT), 0.161064130724201, 1e-14);
-    EXPECT_NEAR(t.get(dSS), -0.975928505799621, 1e-14);
-    EXPECT_NEAR(t.get(dKK), -0.956886455945976, 1e-14);
-    EXPECT_NEAR(t.get(dvv), -6.73743074286942, 1e-14);
-    EXPECT_NEAR(t.get(dTT), -0.312656322438763, 1e-14);
-    EXPECT_NEAR(t.get(dSK), 0.966360579272174, 1e-14);
-    EXPECT_NEAR(t.get(dSv), -50.2111752240705, 2e-14);
-    EXPECT_NEAR(t.get(dST), -7.53167628361058, 1e-14);
-    EXPECT_NEAR(t.get(dKv), 50.7716155370646, 1e-14);
-    EXPECT_NEAR(t.get(dKT), 7.61574233055969, 1e-14);
-    EXPECT_NEAR(t.get(dvT), 0.0631462600642643, 1e-14);
+    EXPECT_NEAR(ct.get(res), 13.171437637423516, 1e-13);
+    EXPECT_NEAR(bp.get(dS), 0.6422853983194291, 1e-14);
+    EXPECT_NEAR(bp.get(dK), -0.54720656926323585, 1e-14);
+    EXPECT_NEAR(bp.get(dv), 43.404665363383245, 1e-14);
+    EXPECT_NEAR(bp.get(dT), 3.8196105519777257, 1e-14);
+    EXPECT_NEAR(bp.get(dSS), 0.014592746558426314, 1e-14);
+    EXPECT_NEAR(bp.get(dSK), -0.015486180021187103, 1e-14);
+    EXPECT_NEAR(bp.get(dSv), -0.20124961109986916, 2e-14);
+    EXPECT_NEAR(bp.get(dST), -0.017709965776788628, 1e-14);
+    EXPECT_NEAR(bp.get(dKK), 0.01643431349187204, 1e-14);
+    EXPECT_NEAR(bp.get(dKv), 0.6564757644670376, 1e-14);
+    EXPECT_NEAR(bp.get(dKT), 0.057769867273099296, 1e-14);
+    EXPECT_NEAR(bp.get(dvv), 8.53118665253345, 1e-13);
+    EXPECT_NEAR(bp.get(dvT), 18.112610570776255, 1e-13);
+    EXPECT_NEAR(bp.get(dTT), -1.461778711353871, 1e-14);
 
     // from sympy import *
     // S = Symbol('S')
@@ -76,8 +75,8 @@ TEST(BlackScholes, Order2) {
 
     // totalvol = v * sqrt(T)
     // d1 = log(S / K) / totalvol + totalvol * 0.5
-    // d2 = d1 + totalvol
-    // result = S * cdf(d1) + K * cdf(d2)
+    // d2 = d1 - totalvol
+    // result = S * cdf(d1) - K * cdf(d2)
 
     // def diff(f, diffs):
     //     fdiff = f
@@ -86,10 +85,10 @@ TEST(BlackScholes, Order2) {
 
     //     return fdiff
 
-    // S0 = 1.01
-    // K0 = 1.02
-    // v0 = 0.15
-    // T0 = 0.5
+    // S0 = 104.
+    // K0 = 98.
+    // v0 = 0.22
+    // T0 = 1.25
 
     // print(lambdify([S, K, v, T], result)(S0, K0, v0, T0))
     // print(lambdify([S, K, v, T], diff(result, (S,)))(S0, K0, v0, T0))
@@ -97,15 +96,15 @@ TEST(BlackScholes, Order2) {
     // print(lambdify([S, K, v, T], diff(result, (v,)))(S0, K0, v0, T0))
     // print(lambdify([S, K, v, T], diff(result, (T,)))(S0, K0, v0, T0))
     // print(lambdify([S, K, v, T], diff(result, (S, S)))(S0, K0, v0, T0))
-    // print(lambdify([S, K, v, T], diff(result, (K, K)))(S0, K0, v0, T0))
-    // print(lambdify([S, K, v, T], diff(result, (v, v)))(S0, K0, v0, T0))
-    // print(lambdify([S, K, v, T], diff(result, (T, T)))(S0, K0, v0, T0))
     // print(lambdify([S, K, v, T], diff(result, (S, K)))(S0, K0, v0, T0))
     // print(lambdify([S, K, v, T], diff(result, (S, v)))(S0, K0, v0, T0))
     // print(lambdify([S, K, v, T], diff(result, (S, T)))(S0, K0, v0, T0))
+    // print(lambdify([S, K, v, T], diff(result, (K, K)))(S0, K0, v0, T0))
     // print(lambdify([S, K, v, T], diff(result, (K, v)))(S0, K0, v0, T0))
     // print(lambdify([S, K, v, T], diff(result, (K, T)))(S0, K0, v0, T0))
+    // print(lambdify([S, K, v, T], diff(result, (v, v)))(S0, K0, v0, T0))
     // print(lambdify([S, K, v, T], diff(result, (v, T)))(S0, K0, v0, T0))
-}
+    // print(lambdify([S, K, v, T], diff(result, (T, T)))(S0, K0, v0, T0))
 
-} // namespace adhoc4
+    return 0;
+}
--- test/test_tools.hpp
+++ test/test_tools.hpp
@@ -3,6 +3,7 @@
 
 #include <cmath>
 #include <iostream>
+#include <limits>
 
 template <class D> inline bool expect_near(D val1, D val2, D tol) {
     return std::abs(val1 - val2) < tol;
@@ -10,8 +11,10 @@ template <class D> inline bool expect_near(D val1, D val2, D tol) {
 
 #define EXPECT_NEAR(VAL1, VAL2, TOL)                                           \
     if (!expect_near(VAL1, VAL2, TOL)) {                                       \
+        std::cout.precision(std::numeric_limits<double>::max_digits10);        \
         std::cout << __FILE__ << ":" << __LINE__ << " Failure" << std::endl;   \
-        std::cout << "Difference exceeds " << TOL << ", where" << std::endl;   \
+        std::cout << "Difference " << std::abs(VAL1 - VAL2) << " exceeds "     \
+                  << TOL << ", where" << std::endl;                            \
         std::cout << "val1 evaluates to " << VAL1 << std::endl;                \
         std::cout << "val2 evaluates to " << VAL2 << std::endl;                \
         return 1;                                                              \
