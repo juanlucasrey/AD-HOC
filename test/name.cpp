b882eea2e6aee4508a6530345af0a923a89b6c03
juanlucasrey <juanlucasrey@icloud.com>
2024-11-06 11:31:01 +0000

    moving name check to statis tests

---
+++ test/name.cpp
@@ -0,0 +1,23 @@
+#include <adhoc.hpp>
+#include <base.hpp>
+#include <name.hpp>
+
+#include <string_view>
+
+int main() {
+    {
+        ADHOC(variablename);
+        constexpr auto res = name(variablename);
+        constexpr std::string_view res_check{"variablename"};
+        static_assert(res_check == std::string_view(res.data(), res.size()));
+    }
+
+    {
+        ADHOC(var);
+        constexpr auto res = name(tgamma(asin(exp(var))));
+        constexpr std::string_view res_check{"tgamma(asin(exp(var)))"};
+        static_assert(res_check == std::string_view(res.data(), res.size()));
+    }
+
+    return 0;
+}
