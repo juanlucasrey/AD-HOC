c4367bc2df3014751cd1cc151cd114598549acab
juanlucasrey <juanlucasrey@icloud.com>
2024-11-07 11:05:34 +0000

    avoid unused variable failure

--- test/differential_operator.t.fail.cpp
+++ test/differential_operator.t.fail.cpp
@@ -9,7 +9,5 @@ int main() {
     static_assert(
         std::same_as<decltype(res), decltype(d<2>(var) * d<2>(var2 * var))>);
 
-    constexpr double minus_one_over_root_two =
-        -1.0 / std::numbers::sqrt2_v<double>;
     return 0;
 }
