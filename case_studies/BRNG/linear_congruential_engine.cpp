5ca374651ff1f0eeeb657068424c38e7fd94d1fe
juanlucasrey <juanlucasrey@icloud.com>
2024-11-28 00:32:46 +0000

    lcg more robust

--- case_studies/BRNG/linear_congruential_engine.cpp
+++ case_studies/BRNG/linear_congruential_engine.cpp
@@ -3,8 +3,65 @@
 
 #include "linear_congruential_engine.hpp"
 
+// #include "double_integer.hpp"
+
+#include <boost/multiprecision/cpp_int.hpp>
+using boost::multiprecision::uint128_t;
+
+#include <cstdint>
+#include <limits>
 #include <random>
 #include <vector>
+
+struct uint128 {
+    std::uint64_t high;
+    std::uint64_t low;
+
+    template <class T>
+    uint128(T v) : high(0), low(static_cast<std::uint64_t>(v)) {}
+
+    template <class T>
+    uint128(T h, T l)
+        : high(static_cast<std::uint64_t>(h)),
+          low(static_cast<std::uint64_t>(l)) {}
+
+    auto operator+=(const uint128 &v) noexcept -> uint128 {
+        auto o = low;
+        low += v.low;
+        high += v.high;
+        high += (low < o);
+        return *this;
+    }
+
+    auto operator+(const uint128 &v) const noexcept -> uint128 {
+        uint128 t(*this);
+        t += v;
+        return t;
+    }
+
+    auto operator<<(int amount) -> uint128 {
+        // uint64_t shifts of >= 64 are undefined, so we will need some
+        // special-casing.
+        return amount >= 64 ? uint128(low << (amount - 64),
+                                      static_cast<std::uint64_t>(0))
+               : amount == 0
+                   ? *this
+                   : uint128((high << amount) | (low >> (64 - amount)),
+                             low << amount);
+    }
+
+    auto operator*(uint128 rhs) noexcept -> uint128 {
+        uint64_t a32 = low >> 32;
+        uint64_t a00 = low & 0xffffffff;
+        uint64_t b32 = rhs.low >> 32;
+        uint64_t b00 = rhs.low & 0xffffffff;
+        uint128 result =
+            uint128(high * rhs.low + low * rhs.high + a32 * b32, a00 * b00);
+        result += uint128(a32 * b00) << 32;
+        result += uint128(a00 * b32) << 32;
+        return result;
+    }
+};
 
 int main() {
     TEST_START;
@@ -13,13 +70,6 @@ int main() {
     {
         std::minstd_rand0 rng1;
         adhoc::minstd_rand0 rng2;
-        TEST_FUNC(compare_rng(rng1, rng2, 100));
-    }
-
-    // check against std minstd_rand
-    {
-        std::minstd_rand rng1;
-        adhoc::minstd_rand rng2;
         TEST_FUNC(compare_rng(rng1, rng2, 100));
     }
 
@@ -35,6 +85,13 @@ int main() {
         TEST_FUNC(check_back_and_fwd(rng, 100));
     }
 
+    // check against std minstd_rand
+    {
+        std::minstd_rand rng1;
+        adhoc::minstd_rand rng2;
+        TEST_FUNC(compare_rng(rng1, rng2, 100));
+    }
+
     // minstd_rand fwd and back
     {
         adhoc::minstd_rand rng;
@@ -45,6 +102,111 @@ int main() {
     {
         adhoc::minstd_rand rng;
         TEST_FUNC(check_back_and_fwd(rng, 100));
+    }
+
+    // check against std ZX81
+    {
+        std::linear_congruential_engine<std::uint_fast16_t, 75, 74, 0> rng1;
+        adhoc::ZX81 rng2;
+        TEST_FUNC(compare_rng(rng1, rng2, 100));
+    }
+
+    // ZX81 fwd and back
+    {
+        adhoc::ZX81 rng;
+        TEST_FUNC(check_fwd_and_back(rng, 100));
+    }
+
+    // ZX81 back and fwd
+    {
+        adhoc::ZX81 rng;
+        TEST_FUNC(check_back_and_fwd(rng, 100));
+    }
+
+    // check against std ranqd1
+    {
+        std::linear_congruential_engine<std::uint_fast32_t, 1664525, 1013904223,
+                                        0>
+            rng1;
+        adhoc::ranqd1 rng2;
+        TEST_FUNC(compare_rng(rng1, rng2, 100));
+    }
+
+    // ranqd1 fwd and back
+    {
+        adhoc::ranqd1 rng;
+        TEST_FUNC(check_fwd_and_back(rng, 100));
+    }
+
+    // ranqd1 back and fwd
+    {
+        adhoc::ranqd1 rng;
+        TEST_FUNC(check_back_and_fwd(rng, 100));
+    }
+
+    // check against std RANDU
+    {
+        std::linear_congruential_engine<std::uint_fast32_t, 65539, 0,
+                                        2147483648>
+            rng1;
+        adhoc::RANDU rng2;
+        TEST_FUNC(compare_rng(rng1, rng2, 100));
+    }
+
+    // RANDU fwd and back
+    {
+        adhoc::RANDU rng;
+        TEST_FUNC(check_fwd_and_back(rng, 100));
+    }
+
+    // RANDU back and fwd
+    {
+        adhoc::RANDU rng;
+        TEST_FUNC(check_back_and_fwd(rng, 100));
+    }
+
+    // check against std Borland
+    {
+        std::linear_congruential_engine<std::uint_fast32_t, 22695477, 1,
+                                        2147483648>
+            rng1;
+        adhoc::Borland rng2;
+        TEST_FUNC(compare_rng(rng1, rng2, 100));
+    }
+
+    // Borland fwd and back
+    {
+        adhoc::Borland rng;
+        TEST_FUNC(check_fwd_and_back(rng, 100));
+    }
+
+    // Borland back and fwd
+    {
+        adhoc::Borland rng;
+        TEST_FUNC(check_back_and_fwd(rng, 100));
+    }
+
+    std::uint64_t val1 =
+        static_cast<std::uint64_t>(std::numeric_limits<std::uint32_t>::max());
+    std::uint64_t val2 =
+        static_cast<std::uint64_t>(std::numeric_limits<std::uint32_t>::max());
+    std::uint64_t val3 = val1 * val2;
+    std::cout << val3 << std::endl;
+
+    {
+        uint128 val11(std::numeric_limits<std::uint64_t>::max());
+        uint128 val12(std::numeric_limits<std::uint64_t>::max());
+        auto val13 = val11 * val12;
+        std::cout << "stop" << std::endl;
+    }
+
+    // typedef double_integer<unsigned long long, unsigned long long> uint128_t;
+
+    {
+        uint128_t val11(std::numeric_limits<std::uint64_t>::max());
+        uint128_t val12(std::numeric_limits<std::uint64_t>::max());
+        uint128_t val13 = val11 * val12;
+        std::cout << val13 << std::endl;
     }
 
     TEST_END;
--- case_studies/BRNG/linear_congruential_engine.hpp
+++ case_studies/BRNG/linear_congruential_engine.hpp
@@ -2,6 +2,7 @@
 #define CASE_STUDIES_BRNG_LINEAR_CONGRUENTIAL_ENGINE
 
 #include <cstdint>
+#include <limits>
 
 namespace adhoc {
 
@@ -74,13 +75,101 @@ constexpr auto modular_multiplicative_inverse(UIntType n, UIntType b)
     }
 }
 
+template <class UIntType>
+constexpr auto modular_multiplicative_inverse_max_plus_one(UIntType b) {
+    UIntType t1 = 0;
+    bool t1_b = true; // true means positive, false negative
+    UIntType t2 = 1;
+    bool t2_b = true; // true means positive, false negative
+    UIntType t3 = 0;
+    bool t3_b = true; // true means positive, false negative
+
+    // awkward first step to avoid overflow
+    UIntType n_minus_one = std::numeric_limits<UIntType>::max();
+    UIntType r = n_minus_one - (b - 1);
+    UIntType q = 1 + r / b;
+    r = r % b;
+
+    // t3 = t1 - q × t2
+    if (t1_b && !t2_b) {
+        t3_b = true;
+        t3 = t1 + q * t2;
+    } else if (!t1_b && t2_b) {
+        t3_b = false;
+        t3 = t1 + q * t2;
+    } else if (t1_b && t2_b) {
+        t3_b = t1 >= q * t2;
+        if (t3_b) {
+            t3 = t1 - q * t2;
+        } else {
+            t3 = q * t2 - t1;
+        }
+    } else {
+        t3_b = q * t2 >= t1;
+        if (t3_b) {
+            t3 = q * t2 - t1;
+        } else {
+            t3 = t1 - q * t2;
+        }
+    }
+
+    UIntType n = b;
+    b = r;
+    t1 = t2;
+    t1_b = t2_b;
+    t2 = t3;
+    t2_b = t3_b;
+
+    while (r != 0) {
+        UIntType q = n / b;
+        r = n % b;
+
+        // t3 = t1 - q × t2
+        if (t1_b && !t2_b) {
+            t3_b = true;
+            t3 = t1 + q * t2;
+        } else if (!t1_b && t2_b) {
+            t3_b = false;
+            t3 = t1 + q * t2;
+        } else if (t1_b && t2_b) {
+            t3_b = t1 >= q * t2;
+            if (t3_b) {
+                t3 = t1 - q * t2;
+            } else {
+                t3 = q * t2 - t1;
+            }
+        } else {
+            t3_b = q * t2 >= t1;
+            if (t3_b) {
+                t3 = q * t2 - t1;
+            } else {
+                t3 = t1 - q * t2;
+            }
+        }
+
+        n = b;
+        b = r;
+        t1 = t2;
+        t1_b = t2_b;
+        t2 = t3;
+        t2_b = t3_b;
+    }
+
+    if (t1_b) {
+        return t1;
+    } else {
+        UIntType q = n_minus_one - (t1 - 1);
+        return q;
+    }
+}
+
 } // namespace detail
 
 template <class UIntType, UIntType a, UIntType c, UIntType m>
 class linear_congruential_engine final {
     static_assert(m == 0 || a < m);
     static_assert(m == 0 || c < m);
-    static_assert(detail::gcd(m, a) == 1U);
+    static_assert(m == 0 || detail::gcd(m, a) == 1U);
 
   public:
     using result_type = UIntType;
@@ -97,7 +186,11 @@ class linear_congruential_engine final {
     inline auto operator()() -> result_type {
         constexpr auto a_64 = static_cast<std::uint_fast64_t>(a);
         constexpr auto c_64 = static_cast<std::uint_fast64_t>(c);
-        constexpr auto m_64 = static_cast<std::uint_fast64_t>(m);
+        constexpr auto m_64 = static_cast<std::uint_fast64_t>(
+            m == 0 ? static_cast<std::uint_fast64_t>(
+                         std::numeric_limits<result_type>::max()) +
+                         1
+                   : static_cast<std::uint_fast64_t>(m));
 
         if constexpr (FwdDirection) {
             this->x = static_cast<UIntType>(
@@ -105,19 +198,24 @@ class linear_congruential_engine final {
                 m_64);
             return this->x;
         } else {
-            constexpr auto a_inv = static_cast<std::uint_fast64_t>(
-                detail::modular_multiplicative_inverse(m, a));
+            constexpr auto a_inv =
+                m == 0 ? detail::modular_multiplicative_inverse_max_plus_one(a)
+                       : detail::modular_multiplicative_inverse(m, a);
+
+            constexpr auto a_inv_64 = static_cast<std::uint_fast64_t>(a_inv);
 
             const auto result = this->x;
             this->x = static_cast<UIntType>(
-                (a_inv * (static_cast<std::uint_fast64_t>(this->x) - c_64)) %
+                (a_inv_64 * (static_cast<std::uint_fast64_t>(this->x) - c_64)) %
                 m_64);
             return result;
         }
     }
 
     static constexpr auto min() -> UIntType { return c == 0U ? 1U : 0U; }
-    static constexpr auto max() -> UIntType { return m - 1U; }
+    static constexpr auto max() -> UIntType {
+        return m == 0 ? std::numeric_limits<result_type>::max() : m - 1U;
+    }
 
   private:
     UIntType x;
@@ -128,6 +226,17 @@ using minstd_rand0 =
 
 using minstd_rand =
     linear_congruential_engine<std::uint_fast32_t, 48271, 0, 2147483647>;
+
+using ZX81 = linear_congruential_engine<std::uint_fast16_t, 75, 74, 0>;
+
+using ranqd1 =
+    linear_congruential_engine<std::uint_fast32_t, 1664525, 1013904223, 0>;
+
+using RANDU =
+    linear_congruential_engine<std::uint_fast32_t, 65539, 0, 2147483648>;
+
+using Borland =
+    linear_congruential_engine<std::uint_fast32_t, 22695477, 1, 2147483648>;
 
 } // namespace adhoc
 
