f126bada44ce3ecde40a3f3b48d00e2a562c70aa
juanlucasrey <juanlucasrey@icloud.com>
2026-03-27 13:33:34 +0000

    licenc

--- dynamic/tests/credit/array_operators.hpp
+++ dynamic/tests/credit/array_operators.hpp
@@ -1,3 +1,23 @@
+/*
+ * AD-HOC, Automatic Differentiation for High Order Calculations
+ *
+ * This file is part of the AD-HOC distribution
+ * (https://github.com/juanlucasrey/AD-HOC).
+ * Copyright (c) 2026 Juan Lucas Rey
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #ifndef BMC_TOOLS_ARRAY_OPERATORS
 #define BMC_TOOLS_ARRAY_OPERATORS
 
